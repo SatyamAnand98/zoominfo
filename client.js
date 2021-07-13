8f7e5dbc9e72076bf489d885ec9886274e7030da
SatyamAnand98 <thesatemail@gmail.com>
2021-07-13 17:27:52 +0530

    created client and server

--- client.js
+++ client.js
@@ -1,12 +1,9 @@
 'use strict';
-//Same as the other projects we import fs for reading documents, in this case employees.js json
-const fs = require('fs');
 
-//Importing GRPC and the proto loader
+const fs = require('fs');
 const grpc = require('grpc');
 const loader = require('@grpc/proto-loader');
 
-//reads the proto
 const packageDefinition = loader.loadSync('message.proto', {
   keepCase: false,
   longs: String,
@@ -15,44 +12,33 @@ const packageDefinition = loader.loadSync('message.proto', {
   oneofs: true
 });
 
-//Loads the proto file to be used in constant pkg
 const pkg = grpc.loadPackageDefinition(packageDefinition);
 
-//Creates server
 const PORT = 9000;
 console.log(PORT)
 const client = new pkg.CreateResource(`localhost:${PORT}`, grpc.credentials.createInsecure());
 
-//Reads command from server
-//Use a number for switch option
 const option = parseInt(process.argv[2],11);
 console.log(option)
 
 switch (option) {
     case 1:
-        //Shows the Metadata for user on server side
         sendMetadata(client);
         break;
     case 2:
-        //Unary Call, gets employee by id
         getByBadgeNumber(client);
         break;
     case 3:
-        //Server streaming call where server sends all the employees
         getAll(client);
         break;
     case 4:
-        //Client Streaming request where the client streams to the server
         addPhoto(client);
         break;
     case 5:
-        //Bidirectional streaming
-        //Stream request and stream response back
         saveAll(client);
         break;
 }
 
-//Stream request ti save all these new employees
 function saveAll(client) {
     const employees = [
         {
@@ -71,8 +57,6 @@ function saveAll(client) {
 		}
     ];
 
-
-    //starts the call and open the channel to send and receive
     const call = client.saveAll();
     call.on('data', function (emp) {
         console.log(emp.employee);
@@ -80,7 +64,6 @@ function saveAll(client) {
     employees.forEach(function (emp) {
         call.write({employee: emp});
     });
-    //inform server to close call
     call.end();
 }
 
@@ -88,15 +71,11 @@ function addPhoto(client) {
     const md = new grpc.Metadata();
     md.add('badgenumber', '2080');
 
-    //creates client to send stream message accross (Photo)
-    //return error if any or result if streaming is Ok
     const call = client.addPhoto(md, function (err, result) {
         console.log(result);
     });
 
-    //uses fs to read image file
     const stream = fs.createReadStream('Penguins.jpg');
-    //Chunk data to send it in different pieces
     stream.on('data', function (chunk) {
         call.write({data: chunk});
     });
@@ -105,7 +84,6 @@ function addPhoto(client) {
     });
 }
 
-//starts get all function to retrieve all the employees
 function getAll(client) {
     const call = client.getAll({});
 
@@ -114,7 +92,6 @@ function getAll(client) {
     });
 }
 
-//metadata function
 function sendMetadata(client) {
     const md = new grpc.Metadata();
     md.add('username', 'mvansickle');
@@ -123,7 +100,6 @@ function sendMetadata(client) {
     client.getByBadgeNumber({}, md, function () {});
 }
 
-//Gets employee by ID sending parameters in the call (2080 is the employee batch id)
 function getByBadgeNumber(client) {
     client.getByBadgeNumber({badgeNumber: 2080}, function (err, response) {
         if (err) {
--- server.js
+++ server.js
@@ -19,7 +19,6 @@ const employees = require('./DMS').DMS;
 const PORT = 9000;
 const server = new grpc.Server();
 
-//Services
 server.addService(pkg.CreateResource.service, {
   getByBadgeNumber: getByBadgeNumber,
   getAll: getAll,
@@ -32,7 +31,6 @@ server.bind(`localhost:${PORT}`, grpc.ServerCredentials.createInsecure());
 console.log(`Welcome, the server is running on port ${PORT}`);
 server.start();
 
-//Function on server side to verify batch id and send it to the client
 function getByBadgeNumber(call, callback){
 
   const md = call.metadata.getMap();
@@ -40,10 +38,8 @@ function getByBadgeNumber(call, callback){
         console.log(key, md[key]);
     }
   
-  //pass batch numbr from client request to the constant badge number
   const badgeNumber = call.request.badgeNumber;
 
-    //goes through employees on for loop to get the one where batch number from client request matches one on employees.js
     for (let i = 0; i < employees.length; i++) {
         if (employees[i].badgeNumber === badgeNumber) {
             callback(null, {employee: employees[i]});
@@ -54,7 +50,6 @@ function getByBadgeNumber(call, callback){
   callback('error');
 }
 
-//get all employees function on server side
 function getAll(call){
   employees.forEach(function(emp) {
     call.write({employee: emp});
@@ -63,27 +58,23 @@ function getAll(call){
 call.end();
 }
 
-//stream photo to employee. Processes the chunks of the image
 function addPhoto(call, callback){
   const md = call.metadata.getMap();
     for (let key in md) {
         console.log(key, md[key]);
     }
-    //buffers and add the pieces of the image
     let result = new Buffer(0);
     call.on('data', function (data) {
         result = Buffer.concat([result, data.data]);
         console.log(`Message received with size ${data.data.length}`);
     });
 
-    //Returns the message informing if streaming was successful and inform size in bytes
     call.on('end', function() {
         callback(null, {isOk: true});
         console.log(`Total file size: ${result.length} bytes`);
     })
 }
 
-//bi directional streaming. Client adds new employees holydays and then server returns the message when saved
 function saveAll(call, callback){
     call.on('data', function (emp) {
       employees.push(emp.employee);
@@ -97,7 +88,6 @@ function saveAll(call, callback){
   });
 }
 
-//not in use
 function save(call, callback){
   
 }
