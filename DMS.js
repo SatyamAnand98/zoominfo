e8882352fb7a409cdbf49713f6ad899842d58b9a
SatyamAnand98 <thesatemail@gmail.com>
2021-07-13 17:24:56 +0530

    created client and server

---
+++ DMS.js
@@ -0,0 +1,28 @@
+const DMS = [
+    {
+		id: 1,
+		badgeNumber: 2080,
+		firstName: "Coke",
+		lastName: "Orellana",
+		vacationAccrualRate: 2,
+		vacationAccrued: 30,
+	},
+    {
+		id: 2,
+		badgeNumber: 7538,
+		firstName: "Jorge",
+		lastName: "Orellana",
+		vacationAccrualRate: 2.3,
+		vacationAccrued: 23.4,
+	},
+    {
+		id: 3,
+		badgeNumber: 5144,
+		firstName: "Julianne",
+		lastName: "Lyne",
+		vacationAccrualRate: 3,
+		vacationAccrued: 31.7,
+	}
+];
+
+exports.DMS = DMS;
--- server.js
+++ server.js
@@ -1,27 +1,103 @@
-var PROTO_PATH = __dirname + '/functions/serverFunctions.proto';
-var grpc = require('@grpc/grpc-js');
-var protoLoader = require('@grpc/proto-loader');
-var packageDefinition = protoLoader.loadSync(
-	PROTO_PATH,
-	{keepCase: true,
-	longs: String,
-	enums: String,
-	defaults: true,
-	oneofs: true
-	});
+'use strict';
 
-var server_proto = grpc.loadPackageDefinition(packageDefinition).serverFunctions;
+const fs = require('fs');
+const grpc = require('grpc');
+const loader = require('@grpc/proto-loader');
 
-function sayHello(call, callback){
-	callback(null, {message:'Hello'+call.request.name});
+const packageDefinition = loader.loadSync('message.proto', {
+  keepCase: false,
+  longs: String,
+  enums: String, 
+  defaults: true,
+  oneofs: true
+});
+
+const pkg = grpc.loadPackageDefinition(packageDefinition);
+
+const employees = require('./DMS').DMS;
+
+const PORT = 9000;
+const server = new grpc.Server();
+
+//Services
+server.addService(pkg.CreateResource.service, {
+  getByBadgeNumber: getByBadgeNumber,
+  getAll: getAll,
+  addPhoto: addPhoto,
+  saveAll: saveAll,
+  save: save
+});
+
+server.bind(`localhost:${PORT}`, grpc.ServerCredentials.createInsecure());
+console.log(`Welcome, the server is running on port ${PORT}`);
+server.start();
+
+//Function on server side to verify batch id and send it to the client
+function getByBadgeNumber(call, callback){
+
+  const md = call.metadata.getMap();
+    for (let key in md) {
+        console.log(key, md[key]);
+    }
+  
+  //pass batch numbr from client request to the constant badge number
+  const badgeNumber = call.request.badgeNumber;
+
+    //goes through employees on for loop to get the one where batch number from client request matches one on employees.js
+    for (let i = 0; i < employees.length; i++) {
+        if (employees[i].badgeNumber === badgeNumber) {
+            callback(null, {employee: employees[i]});
+            return;
+        }
+    }
+
+  callback('error');
 }
 
-function main(){
-	var server = new grpc.Server();
-  server.addService(server_proto.EchoService.service, {sayHello: sayHello});
-  server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
-    server.start();
+//get all employees function on server side
+function getAll(call){
+  employees.forEach(function(emp) {
+    call.write({employee: emp});
+});
+
+call.end();
+}
+
+//stream photo to employee. Processes the chunks of the image
+function addPhoto(call, callback){
+  const md = call.metadata.getMap();
+    for (let key in md) {
+        console.log(key, md[key]);
+    }
+    //buffers and add the pieces of the image
+    let result = new Buffer(0);
+    call.on('data', function (data) {
+        result = Buffer.concat([result, data.data]);
+        console.log(`Message received with size ${data.data.length}`);
+    });
+
+    //Returns the message informing if streaming was successful and inform size in bytes
+    call.on('end', function() {
+        callback(null, {isOk: true});
+        console.log(`Total file size: ${result.length} bytes`);
+    })
+}
+
+//bi directional streaming. Client adds new employees holydays and then server returns the message when saved
+function saveAll(call, callback){
+    call.on('data', function (emp) {
+      employees.push(emp.employee);
+      call.write({employee: emp.employee});
+  });
+  call.on('end', function () {
+      employees.forEach(function (emp) {
+          console.log(emp);
+      });
+      call.end();
   });
 }
 
-main();
+//not in use
+function save(call, callback){
+  
+}
